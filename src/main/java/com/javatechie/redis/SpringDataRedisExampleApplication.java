be442cacf89e9d2e37f882dbdef203d1f9152cd3
PovilasBanys <povilas.banys.it@gmail.com>
2023-10-18 16:15:49 +0300

    Com

--- src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
+++ src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+//import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisShardInfo;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -16,30 +19,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 public class SpringDataRedisExampleApplication {
-//    @Autowired
-//    private ProductDao dao;
-//
-//    @PostMapping
-//    public Product save(@RequestBody Product product, HttpServletRequest request) {
-//        return dao.save(product, request);
-//    }
-//
-//    @GetMapping
-//    public List<Product> getAllProducts() {
-//        return dao.findAll();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Product findProduct(@PathVariable int id) {
-//        return dao.findProductById(id);
-//    }
-//    @DeleteMapping("/{id}")
-//    public String remove(@PathVariable int id)   {
-//    	return dao.deleteProduct(id);
-//	}
-
-
-
     @Autowired
     private ProductDao dao;
 
@@ -69,12 +48,12 @@ public class SpringDataRedisExampleApplication {
         return dao.countIp();
     }
 
-    public ModelAndView index() {
-        ModelAndView model = new ModelAndView("index");
-        String addressCount = dao.countIp();
-        model.addObject("addressCount", addressCount);
-        return model;
-    }
+//    public ModelAndView index() {
+//        ModelAndView model = new ModelAndView("index");
+//        String addressCount = dao.countIp();
+//        model.addObject("addressCount", addressCount);
+//        return model;
+//    }
 
     public ModelAndView index(HttpServletRequest request) {
         ModelAndView model = new ModelAndView("index");
@@ -84,6 +63,68 @@ public class SpringDataRedisExampleApplication {
     }
 
     public static void main(String[] args) {
+
+
+
+//		boolean useSsl = true;
+//		JedisShardInfo shardInfo = new JedisShardInfo("dbibmdevopsapp-cache.redis.cache.windows.net", 6380, useSsl);
+//		shardInfo.setPassword("SB0ihmWJOzBxr0VPkEPfRvVW2pBJWHAIxAzCaCDQM6k="); /* Use your access key. */
+//		Jedis jedis = new Jedis(shardInfo);
+//		jedis.set("foo", "bar");
+
+
+
+
+//		boolean useSsl = true;
+//		/* In this line, replace <name> with your cache name: */
+//		JedisShardInfo shardInfo = new JedisShardInfo("dbibmdevopsapp-cache.redis.cache.windows.net", 6380, useSsl);
+//		shardInfo.setPassword("SB0ihmWJOzBxr0VPkEPfRvVW2pBJWHAIxAzCaCDQM6k="); /* Use your access key. */
+//		shardInfo.setConnectionTimeout(9999999);
+//		shardInfo.setSoTimeout(9999999);
+//		Jedis jedis = new Jedis(shardInfo);
+//		jedis.set("foo", "baaar");
+//		String value = jedis.get("foo");
+//        System.out.println(value);
+
+
+
+
+
+//		boolean useSsl = true;
+//		String cacheHostname = System.getenv("dbibmdevopsapp-cache.redis.cache.windows.net");
+//		String cachekey = System.getenv("SB0ihmWJOzBxr0VPkEPfRvVW2pBJWHAIxAzCaCDQM6k=");
+//
+//		// Connect to the Azure Cache for Redis over the TLS/SSL port using the key.
+//		Jedis jedis = new Jedis(cacheHostname, 6380, DefaultJedisClientConfig.builder()
+//				.password(cachekey)
+//				.ssl(useSsl)
+//				.build());
+//
+//		// Perform cache operations using the cache connection object...
+//
+//		// Simple PING command
+//		System.out.println( "\nCache Command  : Ping" );
+//		System.out.println( "Cache Response : " + jedis.ping());
+//
+//		// Simple get and put of integral data types into the cache
+//		System.out.println( "\nCache Command  : GET Message" );
+//		System.out.println( "Cache Response : " + jedis.get("Message"));
+//
+//		System.out.println( "\nCache Command  : SET Message" );
+//		System.out.println( "Cache Response : " + jedis.set("Message", "Hello! The cache is working from Java!"));
+//
+//		// Demonstrate "SET Message" executed as expected...
+//		System.out.println( "\nCache Command  : GET Message" );
+//		System.out.println( "Cache Response : " + jedis.get("Message"));
+//
+//		// Get the client list, useful to see if connection list is growing...
+//		System.out.println( "\nCache Command  : CLIENT LIST" );
+//		System.out.println( "Cache Response : " + jedis.clientList());
+//
+//		jedis.close();
+
+
+
         SpringApplication.run(SpringDataRedisExampleApplication.class, args);
     }
 
