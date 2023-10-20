a9fb0e255436a4b12c73ef9268047f21056f9ce4
PovilasBanys <povilas.banys.it@gmail.com>
2023-10-20 03:21:02 +0300

    committ

--- src/main/java/com/javatechie/redis/config/RedisConfig.java
+++ src/main/java/com/javatechie/redis/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.javatechie.redis.config;
 
+import com.javatechie.redis.SpringDataRedisExampleApplication;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +10,12 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.logging.Logger;
 
 @Configuration
 @EnableRedisRepositories
@@ -23,39 +29,20 @@ public class RedisConfig {
 //        return new JedisConnectionFactory(configuration);
 
 
-    @Value("${redis.host}")
-    private String redisHost;
-
-    @Value("${redis.port}")
-    private int redisPort;
-
-    @Value("${redis.password}")
-    private String redisPassword;
-
-    @Value("${redis.ssl}")
-    private boolean redisSsl;
-
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
-
-        return new LettuceConnectionFactory(configuration);
+        @Bean
+        public JedisConnectionFactory connectionFactory() {
+            RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+            configuration.setHostName("dbibmdevopsapp-cache.redis.cache.windows.net");
+            configuration.setPort(6380);
+            configuration.setPassword("SB0ihmWJOzBxr0VPkEPfRvVW2pBJWHAIxAzCaCDQM6k=");
+            return new JedisConnectionFactory(configuration);
     }
 
-
-//        @Bean
-//        public JedisConnectionFactory connectionFactory() {
-//            RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-//            configuration.setHostName("dbibmdevopsapp-cache.redis.cache.windows.net");
-//            configuration.setPort(6380);
-//            configuration.setPassword("SB0ihmWJOzBxr0VPkEPfRvVW2pBJWHAIxAzCaCDQM6k=");
-//            return new JedisConnectionFactory(configuration);
-//    }
 
     @Bean
     public RedisTemplate<String, Object> template() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new JdkSerializationRedisSerializer());
@@ -64,18 +51,5 @@ public class RedisConfig {
         template.afterPropertiesSet();
         return template;
     }
-
-//    @Bean
-//    public RedisTemplate<String, Object> template() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(connectionFactory());
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setHashKeySerializer(new StringRedisSerializer());
-//        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
-//        template.setValueSerializer(new JdkSerializationRedisSerializer());
-//        template.setEnableTransactionSupport(true);
-//        template.afterPropertiesSet();
-//        return template;
-//    }
 
 }
