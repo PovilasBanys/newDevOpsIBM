c31ccab728679551981402b55db62cade2ee9c1b
PovilasBanys <povilas.banys.it@gmail.com>
2023-10-20 14:57:03 +0300

    artifact

--- src/main/java/com/javatechie/redis/config/RedisConfig.java
+++ src/main/java/com/javatechie/redis/config/RedisConfig.java
@@ -1,21 +1,13 @@
 package com.javatechie.redis.config;
 
-import com.javatechie.redis.SpringDataRedisExampleApplication;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.logging.Logger;
 
 @Configuration
 @EnableRedisRepositories
