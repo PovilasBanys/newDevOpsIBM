8d1f91f4e70b22b261eec4bd1f30c445925970d8
PovilasBanys <povilas.banys.it@gmail.com>
2023-10-23 03:44:29 +0300

    commit gg

--- src/main/java/com/javatechie/redis/config/RedisConfig.java
+++ src/main/java/com/javatechie/redis/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -13,23 +14,28 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Bean
-    public JedisConnectionFactory connectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
-        configuration.setPort(6379);
-        return new JedisConnectionFactory(configuration);
-    }
-
-
-//        @Bean
-//        public JedisConnectionFactory connectionFactory() {
-//            RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-//            configuration.setHostName("dbibmdevopsapp-cache.redis.cache.windows.net");
-//            configuration.setPort(6380);
-//            configuration.setPassword("SB0ihmWJOzBxr0VPkEPfRvVW2pBJWHAIxAzCaCDQM6k=");
-//            return new JedisConnectionFactory(configuration);
+//    @Bean
+//    public JedisConnectionFactory connectionFactory() {
+//        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+//        configuration.setHostName("localhost");
+//        configuration.setPort(6379);
+//        return new JedisConnectionFactory(configuration);
 //    }
+
+
+
+
+
+
+        @Bean
+        public JedisConnectionFactory connectionFactory() {
+            RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+            configuration.setHostName("newDevOpsIBM.redis.cache.windows.net");
+            configuration.setPort(6380);
+            configuration.setPassword("9I9ceDkCA5M4Wl5bxbs2G6ZiVIlsK9F0MAzCaKrHYzc=");
+            configuration.setDatabase(0);
+            return new JedisConnectionFactory(configuration);
+    }
 
 
     @Bean
@@ -44,5 +50,15 @@ public class RedisConfig {
         template.afterPropertiesSet();
         return template;
     }
+
+
+
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+//        template.setConnectionFactory(connectionFactory());
+//        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+//        return template;
+//    }
 
 }
